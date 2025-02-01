9fd83b93ed355ebcd1e78449ccfc981a1f25e818
Victor Ubah <99929155+OfficiallyVictor@users.noreply.github.com>
2025-02-01 08:32:05 -1100

    navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,7 +4,14 @@ import './Navbar.css'
 const Navbar = () => {
   return (
    <header className="header">
+    <div className='header'>
     <p>Qovai</p>
+    <div className="headerside">
+        <span>
+            <p></p>
+        </span>
+    </div>
+    </div>
    </header>
   )
 }
